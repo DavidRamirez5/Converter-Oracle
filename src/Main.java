a675b63084babee997af4a574d044a3eccad36e3
David Patiño <david.andrespr5@gmail.com>
2023-08-10 08:54:45 -0600

    Create new class MainWindow

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import Views.MainWindow;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Starting ......");
+
+        MainWindow window = new MainWindow();
+        window.welcome();
     }
 }
