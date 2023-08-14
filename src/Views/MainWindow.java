77c73e516f98e7b15f68957890aadee22c82a513
David Patiño <david.andrespr5@gmail.com>
2023-08-14 08:38:50 -0600

    Modify method welecom()

--- src/Views/MainWindow.java
+++ src/Views/MainWindow.java
@@ -4,9 +4,25 @@ import javax.swing.*;
 
 public class MainWindow {
     private Object [] options = {"Divisas","Temperatura"};
+    private Object option;
+    private int exit;
 
     public void welcome(){
-        JOptionPane.showInputDialog(null,"Selecciona la Conversion que deseas realizar","Convertidor Alura-Oracle",
-                JOptionPane.QUESTION_MESSAGE,null,options,options[0]);
+      do {
+          option = JOptionPane.showInputDialog(null, "Selecciona la Conversion que deseas realizar", "Convertidor Alura-Oracle",
+                  JOptionPane.QUESTION_MESSAGE, null, options, options[0]);
+
+          if (option == "Divisas") {
+              CurrenciesWindow curriences = new CurrenciesWindow();
+              curriences.menu();
+          } else {
+              JOptionPane.showMessageDialog(null, "En construccion ......");
+          }
+
+          exit= JOptionPane.showConfirmDialog(null,"¿Desea realizar otra conversion?");
+
+      }while(exit == 0);
+
     }
+
 }
